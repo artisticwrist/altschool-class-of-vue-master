6f73396143458e6b9872a1d9e26a3c66a64b52e4
Joseph George <georgejoeemmanuel@gmail.com>
2023-02-07 16:18:58 -0800

    Changed the colorPickerGame logic into a composable named comsable

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script>
+
+    import ColorManager from "../composables/composable"
+
+    export default {
+      setup() {
+        const { colors, message, matchColor } = ColorManager();
+        return {
+          colors,
+          message, 
+          matchColor 
+        } 
+      }
+    };
+</script>
+
+<style></style>
